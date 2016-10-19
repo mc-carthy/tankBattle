bc5c79ff061ee8d3dbeb254813015f21f5966892
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-19 18:21:25 +0100

    Track pointer protection

--- tankBattle/Source/tankBattle/Private/TankMovementComponent.cpp
+++ tankBattle/Source/tankBattle/Private/TankMovementComponent.cpp
@@ -7,22 +7,26 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet)
-	{
-		return;
-	}
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	if (!LeftTrack || !RightTrack)
+	{
+		return;
+	}
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
+	if (!LeftTrack || !RightTrack)
+	{
+		return;
+	}
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
