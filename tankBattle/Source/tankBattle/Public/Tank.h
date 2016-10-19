413ac3f5cba59276b5b35066c1d5caa45539c85e
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-19 15:58:02 +0100

    Replace EditAnywhere with EditDefaultsOnly to ensure all blueprints have the same parameters

--- tankBattle/Source/tankBattle/Public/Tank.h
+++ tankBattle/Source/tankBattle/Public/Tank.h
@@ -38,14 +38,14 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	UPROPERTY(EditAnywhere, Category = Firing)
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float LaunchSpeed = 5000;
 
-	UPROPERTY(EditAnywhere, Category = Setup)
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
 	TSubclassOf<AProjectile> ProjectileBlueprint;
 
 	UTankBarrel* Barrel = nullptr;
-
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float ReloadTimeInSeconds = 3;
 	double LastFireTime = 0;
 	
--- tankBattle/Source/tankBattle/Public/TankPlayerController.h
+++ tankBattle/Source/tankBattle/Public/TankPlayerController.h
@@ -21,11 +21,11 @@ private:
 	void AimTowardsCrosshair();
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrosshairXLocation = 0.5f;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrosshairYLocation = 0.3333f;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float LineTraceRange = 1000000;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
