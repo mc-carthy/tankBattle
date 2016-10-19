adba5a1c03639495778cc43612e7dead7772ef6b
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-19 23:13:11 +0100

    Feed DotProduct into IntendMoveForward AI function to allow AI to pull up alongside player tank

--- tankBattle/Source/tankBattle/Private/TankMovementComponent.cpp
+++ tankBattle/Source/tankBattle/Private/TankMovementComponent.cpp
@@ -33,7 +33,9 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString)
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
 }
