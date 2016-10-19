4eedbf3b73804cae0b7591d5b3ce1fed54fbf1f4
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-19 18:17:23 +0100

    Get rotational movement responding via fly-by-wire system on controller

--- tankBattle/Source/tankBattle/Public/TankMovementComponent.h
+++ tankBattle/Source/tankBattle/Public/TankMovementComponent.h
@@ -21,6 +21,8 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
 
 private:
 	UTankTrack* LeftTrack = nullptr; 
