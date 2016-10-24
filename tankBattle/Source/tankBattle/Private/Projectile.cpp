dc01866ce87222020e9a7f4ae4ef4a9f6601a22a
mc-carthy <mccarthy.mick0012@gmail.com>
2016-10-24 14:24:27 +0100

    Lay groundwork for particle systems

--- tankBattle/Source/tankBattle/Private/Projectile.cpp
+++ tankBattle/Source/tankBattle/Private/Projectile.cpp
@@ -11,6 +11,14 @@ AProjectile::AProjectile()
 	PrimaryActorTick.bCanEverTick = true;
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
 	ProjectileMovement->bAutoActivate = false;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
+	SetRootComponent(CollisionMesh);
+	CollisionMesh->SetNotifyRigidBodyCollision(true);
+	CollisionMesh->SetVisibility(false);
+
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
+	LaunchBlast->AttachTo(RootComponent);
 }
 
 // Called when the game starts or when spawned
--- tankBattle/Source/tankBattle/Public/TankAimingComponent.h
+++ tankBattle/Source/tankBattle/Public/TankAimingComponent.h
@@ -33,7 +33,7 @@ public:
 
 	EFiringState GetFiringState() const;
 	UFUNCTION(BlueprintCallable, Category = "Firing")
-	int GetRoundsLeft() const;
+	int32 GetRoundsLeft() const;
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
@@ -59,5 +59,6 @@ private:
 		float ReloadTimeInSeconds = 3;
 	double LastFireTime = 0;
 	FVector AimDirection;
-	int RoundsLeft = 3;
+	UPROPERTY(EditDefaultsOnly, Category = "Firing")
+	int32 RoundsLeft = 3;
 };
